1b5604d43a6637a8dffb5558a229a91c588cd958
andynadal <andy@nadalapps.com>
2021-01-11 13:11:16 -0600

    Initial Commit

---
+++ LocalizedProtocol/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LocalizedProtocol
+//
+//  Created by Andy Nadal on 1/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LocalizedProtocol/LocalizedProtocolApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizedProtocolApp.swift
+//  LocalizedProtocol
+//
+//  Created by Andy Nadal on 1/11/21.
+//
+
+import SwiftUI
+
+@main
+struct LocalizedProtocolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
